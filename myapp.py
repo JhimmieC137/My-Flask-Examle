263a614aec3a711bb5e48fa9d9ca584c8ee75a77
JhimmieC137 <toluwalope.david@gmail.com>
2023-01-13 15:23:59 +0100

    created flask app

---
+++ myapp.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    value = "name"
+    name = "Olujimi David"
+    age = 24
+    slack_username = "jimi"
+    adult = False
+    if age > 18:
+        adult = True
+    return jsonify({
+        value: name,
+        "age": age,
+        "username": slack_username,
+        "legal":adult
+    })
